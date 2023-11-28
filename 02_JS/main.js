979d8b75b82462e2601258e2c14ba388dac43a86
Kelebra <ricardokelebra@gmail.com>
2023-11-27 19:54:33 -0600

    intro a JS

---
+++ 02_JS/main.js
@@ -0,0 +1,53 @@
+// alert('Hola de js')
+
+// Declarar una variable
+//  var nombre = dato
+var Saludo = 'Hola DESDE JS';
+
+// booleano -> true o false 
+// string o texto -> '' o ""
+// number -> 10
+
+
+// salidas o output
+// alert('texto')
+// console.log(info)
+
+// alert(Saludo)
+console.log(Saludo);
+
+
+// entradas o inputs
+// var question = prompt('ingresa tu nombre');
+
+// console.log(question);
+
+
+// operaciones
+
+// sumar +
+// restar -
+// divirdir /
+// multiplicar *
+
+var num1 = 10;
+var num2 = 80;
+
+console.log(num1 + num2);
+
+
+// concatenacion
+
+var nombre = 'Ricardo';
+var apellido = 'Basurto'
+
+console.log(nombre + ' ' + apellido)
+
+
+
+// 1.- Preguntar al usuario 2 numero que quiera sumar y montrarlo en la consola con un mensaje adecuad0
+// en la consola -> el numero "" sumado con el numero "" es : "", hacer la resta
+
+
+// 2.- Preguntar al usuario nombre, apellido, edad, direccion y prefecion monstralo en la consola de una forma adecuena
+// "" tiene "" años de edad, vive en "" y se dedica a ""
